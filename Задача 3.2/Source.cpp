7ea9a644813e0229496f32cbcdaa5275a574ffe3
Patik <51072318+Zubairovva@users.noreply.github.com>
2020-05-20 16:24:15 +0300

    Add files via upload

---
+++ Задача 3.2/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RUS");
+    
+	string text;
+    int position = -2;
+	cout << "Enter a word in English*:\n";
+	cin >> text;
+	for(int i=0, limit= text.size(); i < limit; ++i)
+	{
+		if(text[i] == 'f' || text[i] == 'F')
+		{
+			++position;
+			if(position == 0)
+			{
+				position = i;
+				break;
+			}
+		}
+	}
+	cout << "Index of this entry: " << position << "\n";  
+    system("pause");
+}
